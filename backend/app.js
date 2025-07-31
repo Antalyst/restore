a8d9e5bde3bed64c2a473cbeff3c8181af45efd8
Antalyst <delux385@gmail.com>
2025-07-31 20:22:02 +0800

    WIP: local changes before pulling

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const app = express();
+const path = require('path');
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +15,10 @@ app.use(cors({
 }));
 
 const authRoutes = require('./api/route/auth');
+const itemRoutes = require('./api/route/items');
+
+app.use('/api/items', itemRoutes);
 app.use('/api/auth', authRoutes);
+
 
 module.exports = app;
