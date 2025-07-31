0a7777969b1728208f190b543dbd253550f06cb1
Antalyst <delux385@gmail.com>
2025-07-31 02:00:03 -0700

    update backend

--- backend/api/route/item.js
+++ backend/api/route/item.js
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
   }
-});
+
 const upload = multer({ storage });
 
 
